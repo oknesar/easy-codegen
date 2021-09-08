de0c5c76b65c043505d43143b8d2a40857f72c65
Vladyslav <vlad.rasenko@gmail.com>
2021-09-08 13:27:06 +0300

    [feature] workingDir for the controller

--- src/TemplateController/index.ts
+++ src/TemplateController/index.ts
@@ -4,12 +4,23 @@ import path from 'path'
 interface TemplateControllerSettings {
   name: string
   source: string
+  workingDir?: string
 }
 
-export default class TemplateController {
-  private templateClass: AbstractTemplate<any>
+export default class TemplateController<Variables extends object = any> {
+  private templateClass: AbstractTemplate<Variables>
+  private workingDir: string
 
-  constructor({ name, source }: TemplateControllerSettings) {
+  constructor({ name, source, workingDir = '' }: TemplateControllerSettings) {
+    this.resolveWorkingDir(workingDir)
+    this.initTemplate(name, source)
+  }
+
+  private resolveWorkingDir(workingDir: string) {
+    this.workingDir = path.isAbsolute(workingDir) ? workingDir : path.resolve(process.cwd(), workingDir)
+  }
+
+  private initTemplate(name: string, source: string) {
     let templateClass = require(path.resolve(source, name)).default
     templateClass = templateClass?.default ?? templateClass
     if (!templateClass) {
@@ -18,6 +29,6 @@ export default class TemplateController {
     if (!AbstractTemplate.isPrototypeOf(templateClass)) {
       throw new Error(`Template "${name}" doesn't extend AbstractTemplate`)
     }
-    this.templateClass = new (templateClass as new () => AbstractTemplate<any>)()
+    this.templateClass = new (templateClass as new () => AbstractTemplate<Variables>)()
   }
 }
