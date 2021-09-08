b945359bd2bcc7b54caa56bc935544614678de52
Vladyslav <vlad.rasenko@gmail.com>
2021-09-08 16:11:12 +0300

    [feature] bin file

--- src/TemplateController/index.ts
+++ src/TemplateController/index.ts
@@ -19,8 +19,8 @@ export default class TemplateController<Variables extends object = any> {
   }
 
   private initTemplate(name: string, source: string) {
-    let templateClass = require(path.resolve(source, name)).default
-    templateClass = templateClass?.default ?? templateClass
+    let templateClass = require(path.resolve(source, name))
+    templateClass = templateClass.default ?? templateClass
     if (!templateClass) {
       throw new Error(`Template "${name}" is not found at "${source}"`)
     }
